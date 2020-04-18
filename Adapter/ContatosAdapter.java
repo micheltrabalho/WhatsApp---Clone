d97256db16580026d4e27dfecf023863d94166bf
micheltrabalho <49286801+micheltrabalho@users.noreply.github.com>
2020-04-18 09:30:12 -0300

    Update ContatosAdapter.java

--- Adapter/ContatosAdapter.java
+++ Adapter/ContatosAdapter.java
@@ -3,7 +3,6 @@ package br.com.michel.android.whatsapp.adapter;
 import android.content.Context;
 import android.net.Uri;
 import android.view.LayoutInflater;
-import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -15,63 +14,77 @@ import java.util.List;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import br.com.michel.android.whatsapp.R;
+import br.com.michel.android.whatsapp.model.COnversa;
+import br.com.michel.android.whatsapp.model.Grupo;
 import br.com.michel.android.whatsapp.model.Usuario;
 import de.hdodenhof.circleimageview.CircleImageView;
 
-public class ContatosAdapter extends RecyclerView.Adapter<ContatosAdapter.MyViewHolder> {
+public class ConversasAdapter extends RecyclerView.Adapter<ConversasAdapter.MyViewHolder> {
 
-
-    private List<Usuario> contatos;
+    private List<COnversa> cOnversas;
     private Context context;
 
-    public ContatosAdapter(List<Usuario> listaContato, Context c) {
-        this.contatos = listaContato;
+    public ConversasAdapter(List<COnversa> list, Context c) {
+        this.cOnversas = list;
         this.context = c;
     }
 
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View itemLista = LayoutInflater.from(parent.getContext()).inflate(R.layout.adapter_contatos, parent, false);
-        return new MyViewHolder(itemLista);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.adapter_contatos, parent, false);
+        return new MyViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        COnversa cOnversa = cOnversas.get(position);
+        holder.ultimaMensagem.setText(cOnversa.getUltimamsg());
 
-        Usuario usuario = contatos.get(position);
+        if(cOnversa.getIsgrupo().equals("true")){
+            Grupo grupo = cOnversa.getGrupo();
+            holder.nome.setText(grupo.getNome());
 
-        holder.nome.setText(usuario.getNome());
-        holder.email.setText(usuario.getEmail());
-
-        if (usuario.getFoto() != null){
-
-            Uri uri = Uri.parse(usuario.getFoto());
-            Glide.with(context).load(uri).into(holder.foto);
-
+            if (grupo.getFoto() != null){
+                Uri url = Uri.parse(grupo.getFoto());
+                Glide.with(context).load(url).into(holder.foto);
+            }else {
+                holder.foto.setImageResource(R.drawable.padrao);
+            }
         }else {
+            Usuario usuario = cOnversa.getUsuario();
+            if(usuario != null){
+                holder.nome.setText(usuario.getNome());
 
-            holder.foto.setImageResource(R.drawable.padrao);
+                if (usuario.getFoto() != null){
+
+                    Uri url = Uri.parse(usuario.getFoto());
+                    Glide.with(context).load(url).into(holder.foto);
+                }else {
+                    holder.foto.setImageResource(R.drawable.padrao);
+                }
+            }
+
+
+
         }
     }
 
     @Override
     public int getItemCount() {
-
-        return contatos.size();
+        return  cOnversas.size();
     }
 
-    public class MyViewHolder extends RecyclerView.ViewHolder{
-
+    public class MyViewHolder extends RecyclerView.ViewHolder {
         CircleImageView foto;
-        TextView nome, email;
+        TextView nome, ultimaMensagem;
 
-        public MyViewHolder(View itemView){
-            super(itemView);
+        public MyViewHolder(View itemview) {
+            super(itemview);
 
-            foto = itemView.findViewById(R.id.imageCirculecontato);
-            nome = itemView.findViewById(R.id.textViewNomeContato);
-            email = itemView.findViewById(R.id.textViewEmailCOntato);
+            foto = itemview.findViewById(R.id.imageCirculecontato);
+            nome = itemview.findViewById(R.id.textViewNomeContato);
+            ultimaMensagem = itemview.findViewById(R.id.textViewEmailCOntato);
         }
     }
 }
